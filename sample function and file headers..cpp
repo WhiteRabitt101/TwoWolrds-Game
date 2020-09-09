3f7abcd3b330c0d70a58bfa6e83e696eeff5e47a
joaniem <mcdonoughj@nicc.edu>
2020-09-09 16:19:04 -0500

    Add files via upload

---
+++ sample function and file headers..cpp
@@ -0,0 +1,38 @@
+/*************************************************
+File name: sample function and file headers.cpp
+Date:      09/09/2020
+
+This file contains a sample header for a function 
+  and a sample header for a file.
+
+
+
+**************************************************/
+
+
+
+/**************************************************
+Function name: Attack
+programmer:
+date: 
+
+This function is to attack another player.
+
+Parameters:
+   player  Another - this is the player being attacked
+   weapon  ChosenWeapon - this is the weapon being used in the attack
+   double  RandomInfluence - factor of how attack affects Another player
+   int     ThisNumber - 
+   
+Return value: void
+
+Assumes: ThisNumber != 0 because it's used as a denominator in a division statement.
+
+**************************************************/
+void Attack( player Another, weapon ChosenWeapon, double RandomInfluence, int ThisNumber)
+{
+		// function code
+
+
+}
+
